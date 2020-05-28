049ce23ed04bdefe2bd7fd4588130e25b59f68e1
rheirman <robbeheirman@msn.com>
2020-05-28 10:03:04 +0200

    Commit

---
+++ Shopify to Nav/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Shopify_to_Nav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new WebServer();
+            server.start();
+        }
+    }
+}
